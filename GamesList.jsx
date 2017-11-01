9af51d60f4f48334fb4a33229c6a16e033769db4
Kacper <kacper133@gmail.com>
2017-11-01 18:10:58 +0100

    Table of contents template

--- GamesList.jsx
+++ GamesList.jsx
@@ -5,13 +5,209 @@ import { Link } from 'react-router-dom'
 class GamesList extends React.Component {
     render() {
         return (
-            <div className="content">
-                <h1 className="content-subhead">#</h1>
-                <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><Link to="/tysiac" className="pure-menu-link">
-                        1000 (zob. Tysiąc) - gra dla 2 [3 i 4] osób
-                    </Link></li>
-                </ul>
+            <div>
+                <div className="content">
+                    <div class="pure-menu pure-menu-horizontal pure-menu-scrollable">
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="##">#</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#A">A</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#B">B</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#C">C</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#D">D</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#E">E</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#F">F</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#G">G</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#H">H</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#I">I</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#J">J</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#K">K</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#L">L</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#M">M</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#N">N</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#O">O</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#P">P</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#Q">Q</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#R">R</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#S">S</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#T">T</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#U">U</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#V">V</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#W">W</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#X">X</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#Y">Y</a>
+                            </li>
+                            <li class="pure-menu-item">
+                                <a className="pure-menu-link" href="#Z">Z</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="content">
+                    <h1 className="content-subhead">
+                        <a name="#"></a>
+                        #
+                    </h1>
+                    <ul className="pure-menu-list">
+                        <li className="pure-menu-item"><Link to="/tysiac" className="pure-menu-link">
+                            1000 (zob. Tysiąc) - gra dla 2 [3 i 4] osób
+                        </Link></li>
+                    </ul>
+
+                    <h1 className="content-subhead">
+                        <a name="A"></a>
+                        A
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="B"></a>
+                        B
+                    </h1><h1 className="content-subhead">
+                        <a name="C"></a>
+                        C
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="D"></a>
+                        D
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="E"></a>
+                        E
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="F"></a>
+                        F
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="G"></a>
+                        G
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="H"></a>
+                        H
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="I"></a>
+                        I
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="J"></a>
+                        J
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="K"></a>
+                        K
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="L"></a>
+                        L
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="M"></a>
+                        M
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="N"></a>
+                        N
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="O"></a>
+                        O
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="P"></a>
+                        P
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="Q"></a>
+                        Q
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="R"></a>
+                        R
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="S"></a>
+                        S
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="T"></a>
+                        T
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="U"></a>
+                        U
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="V"></a>
+                        V
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="W"></a>
+                        W
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="X"></a>
+                        X
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="Y"></a>
+                        Y
+                    </h1>
+                    <h1 className="content-subhead">
+                        <a name="Z"></a>
+                        Z
+                    </h1>
+                </div>
             </div>
         );
     }
