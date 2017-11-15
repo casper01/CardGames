913ff7be40830a15aa70e6794fc094b7f3e019f6
Kacper <kacper133@gmail.com>
2017-11-15 17:47:13 +0100

    New header

--- Header.jsx
+++ Header.jsx
@@ -16,8 +16,13 @@ class Header extends React.Component {
     render() {
         return (
             <div className="header cursor-hand" onClick={this.goToMainPage}>
-                <h1>{ this.props.mainHeader }</h1>
-                <h2>{ this.props.subHeader }</h2>
+                <div className="center-outer-container">
+                    <div className="center-inner-container">
+                        <img className="pure-img" src="images/header.gif"></img>
+                    </div>
+                </div>
+                {/* <h1>{ this.props.mainHeader }</h1> */}
+                <h2>{this.props.subHeader}</h2>
             </div>
         );
     }
